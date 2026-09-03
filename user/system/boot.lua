8eefcb85f125fd0ba6d1084d628fb139af49ddcb
YellowYotu <danieltarasuk@icloud.com>
2026-09-03 21:55:47 +0300

    Load YellowOS services on boot

--- user/system/boot.lua
+++ user/system/boot.lua
@@ -1,4 +1,8 @@
 YellowOS.loadFile("/system/common.lua")
+YellowOS.loadFile("/system/settings.lua")
+YellowOS.loadFile("/system/http.lua")
+YellowOS.loadFile("/system/updater.lua")
+YellowOS.loadFile("/system/network.lua")
 
 local common = YellowOS.common
 local gpu = common.gpu
@@ -12,6 +16,8 @@ gpu.set(3, 5, YellowOS.edition)
 gpu.set(3, 6, YellowOS.device)
 gpu.set(3, 7, "Version " .. YellowOS.version)
 gpu.setForeground(0x808080)
-gpu.set(3, 10, "Starting YellowOS...")
-computer.pullSignal(0.7)
+gpu.set(3, 10, "Starting services...")
+computer.pullSignal(0.4)
+gpu.set(3, 11, "Network and updater ready.")
+computer.pullSignal(0.4)
 YellowOS.loadFile("/system/main.lua")
