07974ecbbf1ca691f73451b92405e9e2ff6f671a
YellowYotu <danieltarasuk@icloud.com>
2026-09-03 21:55:28 +0300

    Add account placeholder

---
+++ user/system/apps/account.lua
@@ -0,0 +1,2 @@
+local common = YellowOS.common
+common.message("Account", "Yellow Account backend is not enabled yet.\n\nDevice ID:\n" .. computer.address() .. "\n\nRegistration and phone sync will be added with the server account service.")
