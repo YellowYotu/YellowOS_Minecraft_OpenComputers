417a90fb8dc3f6bd9459e94a177496b8ff336b4a
YellowYotu <danieltarasuk@icloud.com>
2026-09-03 22:20:04 +0300

    Add Server Edition main menu

---
+++ server_os/system/main.lua
@@ -0,0 +1,43 @@
+local common = YellowOS.common
+local computer = rawget(_G, "computer")
+
+if not computer then
+    computer = require("computer")
+end
+
+local items = {
+    {name = "Files", path = "/system/apps/files.lua"},
+    {name = "Update All Clients", path = "/system/apps/update_clients.lua"},
+    {name = "Flash User HDD", path = "/system/apps/flash_user.lua"},
+    {name = "Update Server", path = "/system/apps/update_server.lua"},
+    {name = "Settings", path = "/system/apps/settings.lua"},
+    {name = "System Information", path = "/system/apps/info.lua"},
+    {name = "Reboot"},
+    {name = "Shutdown"}
+}
+
+while true do
+    local names = {}
+
+    for _, item in ipairs(items) do
+        table.insert(names, item.name)
+    end
+
+    local selected = common.menu("Server Edition " .. YellowOS.version, "YellowOS Control Center", names)
+
+    if selected then
+        local item = items[selected]
+
+        if item.path then
+            YellowOS.loadFile(item.path)
+        elseif item.name == "Reboot" then
+            common.clear()
+            common.gpu.set(3, 3, "Rebooting...")
+            computer.shutdown(true)
+        elseif item.name == "Shutdown" then
+            common.clear()
+            common.gpu.set(3, 3, "Shutting down...")
+            computer.shutdown(false)
+        end
+    end
+end
