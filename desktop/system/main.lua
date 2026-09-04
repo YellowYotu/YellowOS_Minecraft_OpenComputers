fc6fdb83a9cdd1d6a9a544f9d990d6f5c0efb114
YellowYotu <danieltarasuk@icloud.com>
2026-09-04 16:32:17 +0300

    Add YellowStore and applications to Desktop

--- desktop/system/main.lua
+++ desktop/system/main.lua
@@ -1,6 +1,16 @@
 local computer=rawget(_G,"computer")
 local c=YellowOS.common
-local items={{"Files","/system/apps/files.lua"},{"Terminal","/system/apps/terminal.lua"},{"Browser","/system/apps/browser.lua"},{"Updates","/system/apps/updates.lua"},{"Settings","/system/apps/settings.lua"},{"About","/system/apps/about.lua"},{"Reboot"},{"Shutdown"}}
+local items={
+ {"Applications","/system/apps/apps.lua"},
+ {"YellowStore","/system/apps/appstore.lua"},
+ {"Files","/system/apps/files.lua"},
+ {"Terminal","/system/apps/terminal.lua"},
+ {"Browser","/system/apps/browser.lua"},
+ {"Updates","/system/apps/updates.lua"},
+ {"Settings","/system/apps/settings.lua"},
+ {"About","/system/apps/about.lua"},
+ {"Reboot"},{"Shutdown"}
+}
 while true do
  local names={}; for _,it in ipairs(items) do table.insert(names,it[1]) end
  local sub="Desktop Edition"
