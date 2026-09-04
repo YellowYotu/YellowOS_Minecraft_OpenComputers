5f98439640d934930723b89485516274e0a77e8c
YellowYotu <danieltarasuk@icloud.com>
2026-09-04 16:34:55 +0300

    Bump Server Edition to 0.2.2

--- server_os/init.lua
+++ server_os/init.lua
@@ -1,57 +1,22 @@
 local component = rawget(_G, "component")
 local computer = rawget(_G, "computer")
-
-if not component then
-    component = require("component")
-end
-
-if not computer then
-    computer = require("computer")
-end
+if not component then component = require("component") end
+if not computer then computer = require("computer") end
 
 local bootAddress = computer.getBootAddress()
-
-if not bootAddress then
-    error("Boot drive not found")
-end
-
+if not bootAddress then error("Boot drive not found") end
 local fs = component.proxy(bootAddress)
 
 local function loadFile(path)
     local handle, reason = fs.open(path, "r")
-
-    if not handle then
-        error("Cannot open " .. path .. ": " .. tostring(reason))
-    end
-
+    if not handle then error("Cannot open " .. path .. ": " .. tostring(reason)) end
     local data = ""
-
-    while true do
-        local chunk = fs.read(handle, 2048)
-
-        if not chunk then
-            break
-        end
-
-        data = data .. chunk
-    end
-
+    while true do local chunk = fs.read(handle, 2048); if not chunk then break end; data = data .. chunk end
     fs.close(handle)
-
     local program, loadReason = load(data, "=" .. path, "t", _ENV)
-
-    if not program then
-        error(loadReason)
-    end
-
+    if not program then error(loadReason) end
     return program()
 end
 
-_G.YellowOS = {
-    fs = fs,
-    loadFile = loadFile,
-    version = "0.2.1",
-    edition = "Server Edition"
-}
-
+_G.YellowOS = {fs=fs,loadFile=loadFile,version="0.2.2",edition="Server Edition"}
 loadFile("/system/boot.lua")
